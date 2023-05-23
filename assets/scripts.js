4eb67984a0afcde2860ce1d8180a700becd8bba5
SimohBZ <simoh1974@gmail.com>
2023-05-23 13:30:16 +0200

    3rd commit product push ok

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/* $(document).ready(function () {
   $(".gallery").mauGallery({
     columns: {
       xs: 1,
@@ -12,4 +12,27 @@ $(document).ready(function () {
     showTags: true,
     tagsPosition: "top",
   });
+});
+*/
+
+// Suppression du code en jQuery
+document.addEventListener("DOMContentLoaded", function () {
+  let gallery = document.querySelectorAll(".gallery");
+  let mauGalleryOptions = {
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  };
+
+  for (let i = 0; i < gallery.length; i++) {
+    mauGallery(gallery[i], mauGalleryOptions);
+  }
 });
